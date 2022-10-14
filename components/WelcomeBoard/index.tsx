d5c479d471159a20d84865ed71f1145ec81b1ea2
zhenshij <jinseok.xyz@gmail.com>
2022-10-14 09:50:16 +0900

    next 버전 수정 및 typed 제거

--- components/WelcomeBoard/index.tsx
+++ components/WelcomeBoard/index.tsx
@@ -1,7 +1,6 @@
 import { useRef } from 'react'
 import { useRenderCanvas } from './useRenderCanvas'
 import siteMetadata from '@/data/siteMetadata'
-import Typed from 'react-typed'
 
 const WelcomeBoard = () => {
   const canvas = useRef<HTMLCanvasElement>()
@@ -16,15 +15,7 @@ const WelcomeBoard = () => {
         id="canvas"
       />
       <div className="relative z-10 flex h-[calc(100vh_-_166px)] items-center justify-center">
-        <div className="cursor-default px-4 text-3xl md:text-4xl">
-          <Typed
-            strings={siteMetadata.text.welcome}
-            typeSpeed={60}
-            backSpeed={60}
-            backDelay={3000}
-            loop
-          />
-        </div>
+        <div className="cursor-default px-4 text-3xl md:text-4xl"></div>
       </div>
     </div>
   )
